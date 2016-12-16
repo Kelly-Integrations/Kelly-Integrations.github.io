0e2151e449413fe942c35b87eab50a9f4ad6df29
Myles Enriquez <enri.myles@gmail.com>
2016-12-16 11:52:09 -0800

    Update file system.

--- _site/js/main.js
+++ _site/js/main.js
@@ -8,3 +8,12 @@ $(window).scroll(function() {
 			}
 		});
 	});
+
+	$(document).on('click', 'a', function(event){
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+});
+console.log("main.js is working!");

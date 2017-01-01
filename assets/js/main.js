e9a258c2db24c14cc45196efae8fbbfd4b9c0647
Myles Enriquez <enri.myles@gmail.com>
2016-12-31 17:54:55 -0800

    Clear out code.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,48 +1,48 @@
 
-	// Animation for sliding events
-	$(window).scroll(function() {
-			$('.item-animated-right').each(function(){
-			var imagePos = $(this).offset().top;
+// Animation for sliding events
+$(window).scroll(function() {
+		$('.item-animated-right').each(function(){
+		var imagePos = $(this).offset().top;
 
-			var topOfWindow = $(window).scrollTop();
-				if (imagePos < topOfWindow + 400) {
-					$(this).addClass("slideRight");
-				}
-			});
-
-			$('.item-animated-left').each(function(){
-			var imagePos = $(this).offset().top;
-
-			var topOfWindow = $(window).scrollTop();
-				if (imagePos < topOfWindow+400) {
-					$(this).addClass("slideLeft");
-				}
-			});
-	});
-
-	
-
-	// Checking scroll for navbar fade away.
-	function checkScroll() {
-		var startY = $('.navbar').height() * 2; // height of navbar (fixed)
-
-		if ( $(window).scrollTop() > startY ) {
-			$('.navbar').addClass('scrolled');
-		} else {
-			$('.navbar').removeClass('scrolled');
-		}
-	}
-
-	if ( $('.navbar').length > 0 ) {
-		$(window).on('scroll load resize', function(){
-			checkScroll();
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow + 400) {
+				$(this).addClass("slideRight");
+			}
 		});
-	}
 
-		// $(document).on('click', 'a', function(event){
-		// 	event.preventDefault();
-		//
-		// 	$('html, body').animate({
-		// 			scrollTop: $( $.attr(this, 'href') ).offset().top
-		// 	}, 500);
-		// )};
+		$('.item-animated-left').each(function(){
+		var imagePos = $(this).offset().top;
+
+		var topOfWindow = $(window).scrollTop();
+			if (imagePos < topOfWindow+400) {
+				$(this).addClass("slideLeft");
+			}
+		});
+});
+
+
+
+// Checking scroll for navbar fade away.
+function checkScroll() {
+	var startY = $('.navbar').height() * 2; // height of navbar (fixed)
+
+	if ( $(window).scrollTop() > startY ) {
+		$('.navbar').addClass('scrolled');
+	} else {
+		$('.navbar').removeClass('scrolled');
+	}
+}
+
+if ( $('.navbar').length > 0 ) {
+	$(window).on('scroll load resize', function(){
+		checkScroll();
+	});
+}
+
+// $(document).on('click', 'a', function(event){
+// 	event.preventDefault();
+//
+// 	$('html, body').animate({
+// 			scrollTop: $( $.attr(this, 'href') ).offset().top
+// 	}, 500);
+// )};

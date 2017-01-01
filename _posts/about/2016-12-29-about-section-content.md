7127012ca7eb7f638f104f49708b104cb7b8cc98
Myles Enriquez <enri.myles@gmail.com>
2016-12-31 17:42:06 -0800

    Update site."

---
+++ _posts/about/2016-12-29-about-section-content.md
@@ -0,0 +1,12 @@
+---
+title: about
+header: About Us!
+sub-header: Who are we?
+categories: about
+---
+
+Kelly Integrations (KI) is a Woman-Owned Business that provides Construction Management Services from Concept and Design Review through Construction Completion and Closeout.  
+
+The company was founded to provide specialized project oversight for large complex Baggage Handling Systems, Buildings and Public Works projects. We have experienced staff to manage the design, installation, testing and commissioning of your project.
+
+KI will provide: project management, coordination, quality assurance/quality control, progress updates, pay application review, change order management, and commissioning -- ensuring that systems are built properly and test-ready. We will meet your every construction need as a Government Agency, Stakeholder, Contractor or Design Professional.
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,48 @@
-$(window).scroll(function() {
-		$('.item-animated').each(function(){
-		var imagePos = $(this).offset().top;
 
-		var topOfWindow = $(window).scrollTop();
-			if (imagePos < topOfWindow+400) {
-				$(this).addClass("slideRight");
-			}
-		});
+	// Animation for sliding events
+	$(window).scroll(function() {
+			$('.item-animated-right').each(function(){
+			var imagePos = $(this).offset().top;
+
+			var topOfWindow = $(window).scrollTop();
+				if (imagePos < topOfWindow + 400) {
+					$(this).addClass("slideRight");
+				}
+			});
+
+			$('.item-animated-left').each(function(){
+			var imagePos = $(this).offset().top;
+
+			var topOfWindow = $(window).scrollTop();
+				if (imagePos < topOfWindow+400) {
+					$(this).addClass("slideLeft");
+				}
+			});
 	});
+
+	
+
+	// Checking scroll for navbar fade away.
+	function checkScroll() {
+		var startY = $('.navbar').height() * 2; // height of navbar (fixed)
+
+		if ( $(window).scrollTop() > startY ) {
+			$('.navbar').addClass('scrolled');
+		} else {
+			$('.navbar').removeClass('scrolled');
+		}
+	}
+
+	if ( $('.navbar').length > 0 ) {
+		$(window).on('scroll load resize', function(){
+			checkScroll();
+		});
+	}
+
+		// $(document).on('click', 'a', function(event){
+		// 	event.preventDefault();
+		//
+		// 	$('html, body').animate({
+		// 			scrollTop: $( $.attr(this, 'href') ).offset().top
+		// 	}, 500);
+		// )};
